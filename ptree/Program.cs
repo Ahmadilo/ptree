4233cd7d768be3e160f0736b4d242a46e6bedece
Ahmadilo <ahmadx222x2@gmail.com>
2025-12-23 19:16:31 +0300

    Refactor Program.cs and update project versioning
    Removed unused directory setting in Program.cs.
    Updated ptree.csproj to include versioning information
    with version set to 1.0.0-alpha and added
    informational version details.

--- ptree/Program.cs
+++ ptree/Program.cs
@@ -12,7 +12,6 @@ namespace ptree
 
         static int Main(string[] args)
         {
-            //Directory.SetCurrentDirectory("C:\\Projects\\Galib\\galab-laravel");
             var root = Options.GetRootCommand();
             return root.Parse(args).Invoke();
         }
